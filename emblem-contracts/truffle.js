e3208f99e588691e0e7bd2227e699c19a8447b9b
Will Galebach <willbach@gmail.com>
2018-08-06 09:51:44 -0500

    Restructured folders and added truffle

---
+++ emblem-contracts/truffle.js
@@ -0,0 +1,14 @@
+module.exports = {
+  networks: {
+      testrpc: {
+          host: "localhost",
+          port: 8545,
+          network_id: "*" // Match any network id
+      },
+      gethtest: {
+          host: "localhost",
+          port: 8545,
+          network_id: 58342
+      }
+  }
+};
